a8517c060a9a4cacf392c336656202584930281f
José Mujica <jose@netavantage.com>
2015-01-23 08:44:58 -0500

    all changes for countdown
    all changes for countdown, css end html

--- js/main.js
+++ js/main.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
         openEffect: "elastic",
         closeEffect: "elastic"
     });
+    $('#time, #timer').countdown({
+    date: 'april 30, 2015',
+    leadingZero: true,
+    htmlTemplate: '<span class=\"time-block\"><span class=\"big\">%d</span><span>Days</span></span><span class=\"time-block\"><span class=\"big\">%h</span><span>Hours</span></span><span class=\"time-block\"><span class=\"big\">%i</span><span>Minutes</span></span><span class=\"time-block last\"><span class=\"big\">%s</span><span>Seconds</span></span>'
+  });
 });
